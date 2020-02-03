6bcbd4815a735edd9a1e347b2004006bc916b44d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-03 15:22:21 -0500

    update sol

--- MarkovClasses.py
+++ MarkovClasses.py
@@ -5,22 +5,24 @@ import SimPy.MarkovClasses as Markov
 
 
 class Patient:
-    def __init__(self, id, trans_matrix):
+    def __init__(self, id, trans_rate_matrix):
 
         self.id = id
-        self.rng = RVGs.RNG(seed=id)
-        self.gillespie = Markov.Gillespie(transition_rate_matrix=trans_matrix)
+        self.transRateMatrix = trans_rate_matrix
         self.stateMonitor = PatientStateMonitor()
 
     def simulate(self, sim_length):
+
+        rng = RVGs.RNG(seed=self.id)
+        gillespie = Markov.Gillespie(transition_rate_matrix=self.transRateMatrix)
 
         t = 0
         if_stop = False
         # while the patient is alive and simulation length is not yet reached
         while not if_stop:
-            dt, new_state_index = self.gillespie.get_next_state(
+            dt, new_state_index = gillespie.get_next_state(
                 current_state_index=self.stateMonitor.currentState.value,
-                rng=self.rng)
+                rng=rng)
 
             if dt is None or dt + t > sim_length:
                 if_stop = True
@@ -56,21 +58,25 @@ class PatientStateMonitor:
 
 
 class Cohort:
-    def __init__(self, id, pop_size, trans_matrix):
-        self.id = id
-        self.patients = []
-        self.cohortOutcomes = CohortOutcomes()
+    def __init__(self, id, pop_size, trans_rate_matrix):
 
-        for i in range(pop_size):
-            patient = Patient(id=id*pop_size + i, trans_matrix=trans_matrix)
-            self.patients.append(patient)
+        self.id = id
+        self.popSize = pop_size
+        self.transRateMatrix = trans_rate_matrix
+        self.cohortOutcomes = CohortOutcomes()
 
     def simulate(self, sim_length):
 
-        for patient in self.patients:
+        patients = []
+        for i in range(self.popSize):
+            patient = Patient(id=self.id*self.popSize + i,
+                              trans_rate_matrix=self.transRateMatrix)
+            patients.append(patient)
+
+        for patient in patients:
             patient.simulate(sim_length=sim_length)
 
-        self.cohortOutcomes.extract_outcomes(self.patients)
+        self.cohortOutcomes.extract_outcomes(patients)
 
 
 class CohortOutcomes:
@@ -80,6 +86,7 @@ class CohortOutcomes:
         self.nTotalStrokes = []
         self.nLivingPatients = None
         self.meanSurvivalTime = None
+        self.meanNumOfStrokes = None
 
     def extract_outcomes(self, simulated_patients):
         for patient in simulated_patients:
@@ -88,9 +95,10 @@ class CohortOutcomes:
                 self.nTotalStrokes.append(patient.stateMonitor.nStrokes)
 
         self.meanSurvivalTime = sum(self.survivalTimes) / len(self.survivalTimes)
+        self.meanNumOfStrokes = sum(self.nTotalStrokes) / len(self.nTotalStrokes)
 
         self.nLivingPatients = PathCls.PrevalencePathBatchUpdate(
-            name = '# of living patients',
+            name='# of living patients',
             initial_size= len(simulated_patients),
             times_of_changes=self.survivalTimes,
             increments=[-1]*len(self.survivalTimes)
--- RunMarkovModel.py
+++ RunMarkovModel.py
@@ -1,73 +1,93 @@
 import MarkovClasses as Cls
-import SimPy.SamplePathClasses as PathCls
-import SimPy.FigureSupport as Fig
+import SimPy.Plots.SamplePaths as Path
+import SimPy.Plots.Histogram as Hist
 import InputData as D
 
-# Markov model without temp state
+# -----------------------------
+# Model with no anticoagulation
+# -----------------------------
 myCohortNoAntiCoag = Cls.Cohort(id=1,
                                 pop_size=D.POP_SIZE,
-                                trans_matrix=D.get_trans_rate_matrix(with_treatment=False))
-# Markov model with temp state
-myCohortWithAntiCoag = Cls.Cohort(id=1,
-                                  pop_size=D.POP_SIZE,
-                                  trans_matrix=D.get_trans_rate_matrix(with_treatment=True))
+                                trans_rate_matrix=D.get_trans_rate_matrix(with_treatment=False))
 
-# simulate all models
+# simulate
 myCohortNoAntiCoag.simulate(sim_length=D.SIMULATION_LENGTH)
-myCohortWithAntiCoag.simulate(sim_length=D.SIMULATION_LENGTH)
 
-
-# sample paths
-PathCls.graph_sample_path(
+# survival curve
+Path.plot_sample_path(
     sample_path=myCohortNoAntiCoag.cohortOutcomes.nLivingPatients,
     title='Survival Curve (Model without anticoagulation)',
     x_label='Time Step (Year)',
     y_label='Number of Surviving Patients'
 )
-PathCls.graph_sample_path(
+
+# histograms of survival times
+Hist.plot_histogram(
+    data=myCohortNoAntiCoag.cohortOutcomes.survivalTimes,
+    title='Histogram of Patient Survival Time\n(Model without anticoagulation)',
+    x_label='Survival Time (Year)',
+    y_label='Count',
+    bin_width=5
+)
+
+# histogram of number of strokes
+Hist.plot_histogram(
+    data=myCohortNoAntiCoag.cohortOutcomes.nTotalStrokes,
+    title='Number of Strokes (Without anticoagulation)',
+    x_label='Number of Strokes',
+    y_label='Count',
+    bin_width=1,
+    x_range=[0, 7]
+)
+
+# print the patient survival time and average number of strokes
+print('Without anticoagulation:')
+print('     Mean survival time for the model (years):',
+      myCohortNoAntiCoag.cohortOutcomes.meanSurvivalTime)
+print('     Mean number of strokes:',
+      myCohortNoAntiCoag.cohortOutcomes.meanNumOfStrokes)
+
+
+# -----------------------------
+# Markov model with anticoagulation
+# -----------------------------
+myCohortWithAntiCoag = Cls.Cohort(id=1,
+                                  pop_size=D.POP_SIZE,
+                                  trans_rate_matrix=D.get_trans_rate_matrix(with_treatment=True))
+
+# simulate
+myCohortWithAntiCoag.simulate(sim_length=D.SIMULATION_LENGTH)
+
+# survival curve
+Path.plot_sample_path(
     sample_path=myCohortWithAntiCoag.cohortOutcomes.nLivingPatients,
     title='Survival Curve (Model with anticoagulation)',
     x_label='Time Step (Year)',
     y_label='Number of Surviving Patients'
 )
 
-
 # histograms of survival times
-Fig.graph_histogram(
-    data=myCohortNoAntiCoag.cohortOutcomes.survivalTimes,
-    title='Histogram of Patient Survival Time\n(Model without anticoagulation)',
-    x_label='Survival Time (Year)',
-    y_label='Count',
-    bin_width=1
-)
-Fig.graph_histogram(
+Hist.plot_histogram(
     data=myCohortWithAntiCoag.cohortOutcomes.survivalTimes,
     title='Histogram of Patient Survival Time\n(Model with anticoagulation)',
     x_label='Survival Time (Year)',
     y_label='Count',
-    bin_width=1
-)
-# histogram of number of strokes
-Fig.graph_histogram(
-    data=myCohortNoAntiCoag.cohortOutcomes.nTotalStrokes,
-    title='Number of Strokes (Without anticoagulation)',
-    x_label='Number of Strokes',
-    y_label='Count',
-    bin_width=1
+    bin_width=5
 )
 
-Fig.graph_histogram(
+# histogram of number of strokes
+Hist.plot_histogram(
     data=myCohortWithAntiCoag.cohortOutcomes.nTotalStrokes,
     title='Number of Strokes (With anticoagulation)',
     x_label='Number of Strokes',
     y_label='Count',
-    bin_width=1
+    bin_width=1,
+    x_range=[0, 7]
 )
 
-# print the patient survival time
-print('Mean survival time for the model without anticoagulation (years):',
-      myCohortNoAntiCoag.cohortOutcomes.meanSurvivalTime)
-print('Mean survival time for the model with anticoagulation (years):',
+print('With anticoagulation:')
+print('     Mean survival time for the model (years):',
       myCohortWithAntiCoag.cohortOutcomes.meanSurvivalTime)
-
+print('     Mean number of strokes:',
+      myCohortWithAntiCoag.cohortOutcomes.meanNumOfStrokes)
 
